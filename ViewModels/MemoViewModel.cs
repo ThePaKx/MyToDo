6f19c62b9ec5904c82a5b50a6d43bf873ce0ed4e
ThePaKx <f68963x@163.com>
2023-05-16 10:57:51 +0800

    添加项目文件。

---
+++ ViewModels/MemoViewModel.cs
@@ -0,0 +1,64 @@
+﻿using MyToDo.Common.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyToDo.ViewModels
+{
+    public class MemoViewModel:BindableBase
+    {
+
+        public MemoViewModel()
+        {
+            MemoDtos = new ObservableCollection<MemoDto>();
+            CreateToDoList();
+            AddCommand = new DelegateCommand(Add);
+        }
+        private bool isRigntDrawerOpen;
+
+        /// <summary>
+        /// 右侧窗口是否展开
+        /// </summary>
+        public bool IsRigntDrawerOpen
+        {
+            get { return isRigntDrawerOpen; }
+            set { isRigntDrawerOpen = value; RaisePropertyChanged(); }
+        }
+
+        /// <summary>
+        /// 添加备忘录
+        /// </summary>
+        private void Add()
+        {
+            IsRigntDrawerOpen = true;
+        }
+
+        public DelegateCommand AddCommand { get; private set; }
+
+        private ObservableCollection<MemoDto> memoDtos;
+
+        public ObservableCollection<MemoDto> MemoDtos
+        {
+            get { return memoDtos; }
+            set { memoDtos = value; }
+        }
+        void CreateToDoList()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                MemoDtos.Add(new MemoDto()
+                {
+                    Title = "标题" + i,
+                    Content = "测试数据..."
+
+                });
+            }
+        }
+
+    }
+}
---
+++ ViewModels/ToDoViewModel.cs
@@ -0,0 +1,63 @@
+﻿using MyToDo.Common.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyToDo.ViewModels
+{
+    internal class ToDoViewModel:BindableBase
+    {
+        public ToDoViewModel()
+        {
+            ToDoDtos = new ObservableCollection<ToDoDto>();
+            CreateToDoList();
+            AddCommand = new DelegateCommand(Add);
+        }
+        private bool isRigntDrawerOpen;
+
+        /// <summary>
+        /// 右侧窗口是否展开
+        /// </summary>
+        public bool IsRigntDrawerOpen
+        {
+            get { return isRigntDrawerOpen; }
+            set { isRigntDrawerOpen = value; RaisePropertyChanged(); }
+        }
+        
+        /// <summary>
+        /// 添加待办
+        /// </summary>
+        private void Add()
+        {
+            IsRigntDrawerOpen=true;
+        }
+
+        public DelegateCommand AddCommand { get; private set; }
+
+        private ObservableCollection<ToDoDto> toDoDtos;
+
+        public ObservableCollection<ToDoDto> ToDoDtos
+        {
+            get { return toDoDtos; }
+            set { toDoDtos = value; }
+        }
+        void CreateToDoList()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                ToDoDtos.Add(new ToDoDto() {  
+                    Title = "标题"+i,
+                    Content = "测试数据..."
+
+                });
+            }
+        }
+
+
+    }
+}
